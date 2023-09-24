ad33f3707a663966c3ef6b7dae009578295273f1
Catslin <aicatslin@gmail.com>
2023-09-24 10:26:40 +0800

    Stop I need to learn bootstrap

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const homeRouter = require('./routes/home')
 const methodOverride = require('method-override')
 const app = express()
 
@@ -13,11 +14,16 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
 
-app.get('/', async (req, res) => {
+app.get('/articles', async (req, res) => {
   const articles = await Article.find().sort({ createdAt: 'desc' })
   res.render('articles/index', { articles: articles })
 })
 
+app.get('/',(req,res) => {
+  res.render('home/index')
+})
+
 app.use('/articles', articleRouter)
+app.use('/home', homeRouter)
 
 app.listen(5000)
